2f09c903b953543527540f069151df6bf83b287e
foreverloop <cjones5709@gmail.com>
2018-01-06 12:01:23 +0000

    amended graph,added python code

--- s_film_stats.py
+++ s_film_stats.py
@@ -6,7 +6,14 @@ num_appears = df['num_appear']
 first_names = df['first_name']
 last_names = df['last_name']
 
+#I do not have access to microsoft excel at home currently
+#so the first and last names are being merged here in python instead
+full_names =[]
+for idx,name in enumerate(first_names):
+  full_names.append(name + last_names[idx])
+
 plt.bar(range(5),num_appears[:5],color='orangered')
+plt.xticks(range(5),full_names[:5])
 plt.ylabel('Number of Appearances')
 plt.title('Most Common Actors')
 plt.show()
