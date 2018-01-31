21f118926bb4071311918a1752472404ab97f814
foreverloop <cjones5709@gmail.com>
2018-01-31 20:17:17 +0000

    added 5 lowest rentals

--- rentals_by_country.sql
+++ rentals_by_country.sql
@@ -7,3 +7,12 @@ INNER JOIN customer ON customer.address_id = address.address_id
 INNER JOIN rental ON rental.customer_id = customer.customer_id
 GROUP BY country.country
 ORDER BY COUNT(rental.rental_id) DESC;
+
+SELECT DISTINCT country.country,COUNT(rental.rental_id)
+FROM country
+INNER JOIN city ON city.country_id = country.country_id
+INNER JOIN address ON address.city_id = city.city_id
+INNER JOIN customer ON customer.address_id = address.address_id
+INNER JOIN rental ON rental.customer_id = customer.customer_id
+GROUP BY country.country
+ORDER BY COUNT(rental.rental_id) ASC LIMIT 5;
